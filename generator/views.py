ad5f45b4148c4ca99a02136fc51ab40ff7cc6dd9
jerrychen <jerry@gmail.com>
2022-10-05 21:30:23 +0800

    完成密碼程式

--- generator/views.py
+++ generator/views.py
@@ -1,12 +1,40 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+import random
+
 # Create your views here.
 
 def password(request):
-    return render(request,'./password.html',{'password':123456})
+    #a~z
+    chars=[chr(c) for c in range(97,123)]    
+    # None ==>False
+    if request.GET.get('uppercase'):
+        chars+=[chr(c).upper() for c in range(97,123)]
+
+    if request.GET.get('numbers'):
+        chars+=list('0123456789')
+  
+    if request.GET.get('special'):
+        chars+=list('@#$%^&*')
+
+    print(chars)
+    # 取得輸入的長度
+    length= eval(request.GET.get('length')) if request.GET.get('input-length')=='' \
+        else eval(request.GET.get('input-length'))
+
+    # 樣本數(不會重複，但不能取樣超過容器大小)
+    #print(random.sample(chars,length))
+    password=''.join([random.choice(chars) for i in range(length)])
+
+    # 變成字串?
+    print(password)
+    print(length)
+
+    return render(request,'./password.html',{'password':password})
 
 def index(request):
     print('Hello Django')
+
    
     return render(request,'./index.html')
